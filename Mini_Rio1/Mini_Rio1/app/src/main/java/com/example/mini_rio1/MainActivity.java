3ee2c9209ba0bd3378f82e0a19c75f21c6e1333b
Rio Chase Espinoza <70341822+rcespinoza@users.noreply.github.com>
2023-03-09 11:59:16 -0500

    Update MainActivity.java
    Added comments

--- Mini_Rio1/Mini_Rio1/app/src/main/java/com/example/mini_rio1/MainActivity.java
+++ Mini_Rio1/Mini_Rio1/app/src/main/java/com/example/mini_rio1/MainActivity.java
@@ -19,23 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
 
+
+    //declare variables
     Button loadImageButton;
     ImageView imageView;
     TextView textView;
     TextView classificationText;
     Uri imageUri;
     ArrayList<String> cat = new ArrayList<>(Arrays.asList("Dog", "Cat", "You"));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         cat.add("Cat");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing variables
         loadImageButton = findViewById(R.id.load_image_button);
         imageView = findViewById(R.id.image_view);
         textView = findViewById(R.id.textView);
         classificationText = findViewById(R.id.classification_text);
 
+        //creating method for the button
 //        @Override
         loadImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,11 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //method for selecting the photo
     private void openGallery() {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE_REQUEST);
     }
 
+    //Method for displaying photo and text
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
@@ -63,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //helper method that returns a random category from the array
     private String printCat() {
         Random r = new Random();
 
