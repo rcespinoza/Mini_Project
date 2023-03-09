9606893903646a2dca26ba5fa9fc6309e4fa4603
Rio Chase Espinoza <70341822+rcespinoza@users.noreply.github.com>
2023-03-09 11:55:02 -0500

    Basic work
    Unzipped file and added information in the readme file

---
+++ Mini_Rio1/Mini_Rio1/app/src/main/java/com/example/mini_rio1/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.mini_rio1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    Button loadImageButton;
+    ImageView imageView;
+    TextView textView;
+    TextView classificationText;
+    Uri imageUri;
+    ArrayList<String> cat = new ArrayList<>(Arrays.asList("Dog", "Cat", "You"));
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        cat.add("Cat");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadImageButton = findViewById(R.id.load_image_button);
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.textView);
+        classificationText = findViewById(R.id.classification_text);
+
+//        @Override
+        loadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE_REQUEST){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+            textView.setText(printCat());
+
+        }
+    }
+
+    private String printCat() {
+        Random r = new Random();
+
+        int index = r.nextInt(cat.size());
+        return cat.get(index);
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Mini_Project
+
+#User: When opening project, you will see a load button, text, and blank whitespace
+
+#click load image an select image from android gallery
+
+#once loaded the image should appear on the general interface and you should see the text change to either cat, dog, or you
+
+#the text is a placeholder for a future project
